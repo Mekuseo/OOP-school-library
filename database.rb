e857216c42a31426c168c07d7d006dab6e41ae43
Chukwuemeka Kanu <mekuseo@gmail.com>
2023-05-15 12:25:09 +0100

    Teacher class created and seperate files created for each class

--- database.rb
+++ database.rb
@@ -30,3 +30,14 @@ class Student < Person
     '¯\(ツ)/¯'
   end
 end
+
+class Teacher < Person
+  def initialize(id, age, specialization, name = 'Unknown', parent_permission: true)
+    super(id, age, name, parent_permission)
+    @specialization = specialization
+  end
+
+  def can_use_services?
+    true
+  end
+end
